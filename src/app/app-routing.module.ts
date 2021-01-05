fb79e9db5a8ee7ef854fb0349876e54bf8395af5
MicheleMallia <mallia.michele@gmail.com>
2021-01-05 16:59:51 +0100

    added routing functionalities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LexiconViewComponent } from './views/lexicon-view/lexicon-view.component';
+import { LoginPageComponent } from './views/login-page/login-page.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'lexiconView', component: LexiconViewComponent},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
