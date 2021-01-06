3ad9e7e77f30e21e03507f7577a1fe8bfaf52557
MicheleMallia <mallia.michele@gmail.com>
2021-01-06 17:55:02 +0100

    added extension page

---
+++ src/app/views/extensions-page/extensions-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extensions-page',
+  templateUrl: './extensions-page.component.html',
+  styleUrls: ['./extensions-page.component.scss']
+})
+export class ExtensionsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
