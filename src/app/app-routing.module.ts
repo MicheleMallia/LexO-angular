1cdfcc450be88818081103e7cbf292f453067bc7
MicheleMallia <mallia.michele@gmail.com>
2021-01-06 17:56:30 +0100

    routes updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExtensionsPageComponent } from './views/extensions-page/extensions-page.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { LexiconViewComponent } from './views/lexicon-view/lexicon-view.component';
 import { LoginPageComponent } from './views/login-page/login-page.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { ProfilePageComponent } from './views/profile-page/profile-page.component';
+import { StatisticsPageComponent } from './views/statistics-page/statistics-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
-  {path: 'lexiconView', component: LexiconViewComponent},
+  {path: 'lexicon', component: LexiconViewComponent},
+  {path: 'statistics', component: StatisticsPageComponent},
+  {path: 'extensions', component: ExtensionsPageComponent},
+  {path: 'user', component: ProfilePageComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
