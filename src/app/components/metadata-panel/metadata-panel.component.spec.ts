27306d4d64413aa9985f8ae69db9aa3fef1a313b
MicheleMallia <mallia.michele@gmail.com>
2021-01-08 17:57:39 +0100

    added metadata panel component

---
+++ src/app/components/metadata-panel/metadata-panel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MetadataPanelComponent } from './metadata-panel.component';
+
+describe('MetadataPanelComponent', () => {
+  let component: MetadataPanelComponent;
+  let fixture: ComponentFixture<MetadataPanelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MetadataPanelComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MetadataPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/metadata-panel/metadata-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metadata-panel',
+  templateUrl: './metadata-panel.component.html',
+  styleUrls: ['./metadata-panel.component.scss']
+})
+export class MetadataPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
