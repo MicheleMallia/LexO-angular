a5580961b41e08196efdac34acfb0b45651c8fbd
MicheleMallia <mallia.michele@gmail.com>
2021-01-14 17:44:59 +0100

    rename module after renaming and adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LexiconViewComponent } from './views/lexicon-view/lexicon-view.component';
+import { LexiconPageComponent } from './views/lexicon-page/lexicon-page.component';
 import { LoginPageComponent } from './views/login-page/login-page.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
@@ -12,19 +12,21 @@ import { ProfilePageComponent } from './views/profile-page/profile-page.componen
 import { LexiconPanelComponent } from './components/lexicon-panel/lexicon-panel.component';
 import { MetadataPanelComponent } from './components/metadata-panel/metadata-panel.component';
 import { ReferencePanelComponent } from './components/reference-panel/reference-panel.component';
+import { SearchPageComponent } from './views/search-page/search-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LexiconViewComponent,
+    LexiconPageComponent,
     LoginPageComponent,
     PageNotFoundComponent,
     HomePageComponent,
     ProfilePageComponent,
     LexiconPanelComponent,
     MetadataPanelComponent,
-    ReferencePanelComponent
+    ReferencePanelComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
