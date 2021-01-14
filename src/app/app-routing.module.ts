7497be4840f71ff152f24cb5becf062954437e2c
MicheleMallia <mallia.michele@gmail.com>
2021-01-14 17:02:54 +0100

    deleted routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ExtensionsPageComponent } from './views/extensions-page/extensions-page.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { LexiconViewComponent } from './views/lexicon-view/lexicon-view.component';
 import { LoginPageComponent } from './views/login-page/login-page.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { ProfilePageComponent } from './views/profile-page/profile-page.component';
-import { StatisticsPageComponent } from './views/statistics-page/statistics-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'lexicon', component: LexiconViewComponent},
-  {path: 'statistics', component: StatisticsPageComponent},
-  {path: 'extensions', component: ExtensionsPageComponent},
   {path: 'user', component: ProfilePageComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
