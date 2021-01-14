637bfc4cc4e4ca05a13bdfa483ff31a07d63fd59
MicheleMallia <mallia.michele@gmail.com>
2021-01-14 17:03:05 +0100

    deleted components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,6 @@ import { LoginPageComponent } from './views/login-page/login-page.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { HomePageComponent } from './views/home-page/home-page.component';
 import { ProfilePageComponent } from './views/profile-page/profile-page.component';
-import { StatisticsPageComponent } from './views/statistics-page/statistics-page.component';
-import { ExtensionsPageComponent } from './views/extensions-page/extensions-page.component';
 import { LexiconPanelComponent } from './components/lexicon-panel/lexicon-panel.component';
 import { MetadataPanelComponent } from './components/metadata-panel/metadata-panel.component';
 import { ReferencePanelComponent } from './components/reference-panel/reference-panel.component';
@@ -24,8 +22,6 @@ import { ReferencePanelComponent } from './components/reference-panel/reference-
     PageNotFoundComponent,
     HomePageComponent,
     ProfilePageComponent,
-    StatisticsPageComponent,
-    ExtensionsPageComponent,
     LexiconPanelComponent,
     MetadataPanelComponent,
     ReferencePanelComponent
