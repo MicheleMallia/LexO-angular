ffd52d348e849353a49e3a5ce5e0ecc18fc968c3
MicheleMallia <mallia.michele@gmail.com>
2021-01-14 17:44:42 +0100

    modified route after renaming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './views/home-page/home-page.component';
-import { LexiconViewComponent } from './views/lexicon-view/lexicon-view.component';
+import { LexiconPageComponent } from './views/lexicon-page/lexicon-page.component';
 import { LoginPageComponent } from './views/login-page/login-page.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { ProfilePageComponent } from './views/profile-page/profile-page.component';
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
-  {path: 'lexicon', component: LexiconViewComponent},
+  {path: 'lexicon', component: LexiconPageComponent},
   {path: 'user', component: ProfilePageComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
