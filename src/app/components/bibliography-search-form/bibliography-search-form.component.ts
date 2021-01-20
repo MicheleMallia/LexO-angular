6da956d48ad1c9fcbe6452e9498339bcde233449
MicheleMallia <mallia.michele@gmail.com>
2021-01-20 16:02:58 +0100

    created bibliography search component

---
+++ src/app/components/bibliography-search-form/bibliography-search-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bibliography-search-form',
+  templateUrl: './bibliography-search-form.component.html',
+  styleUrls: ['./bibliography-search-form.component.scss']
+})
+export class BibliographySearchFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
