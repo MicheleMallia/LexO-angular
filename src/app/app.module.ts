d49df36db385d707707ad572c3a1f660f7b2b12f
MicheleMallia <mallia.michele@gmail.com>
2021-01-25 17:19:59 +0100

    added angular tree component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TreeModule } from '@circlon/angular-tree-component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +40,8 @@ import { DocumentSystemTreeComponent } from './components/lexicon-panel/document
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
