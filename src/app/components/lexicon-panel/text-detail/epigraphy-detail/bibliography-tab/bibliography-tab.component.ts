5f0ee724bac2800aea1d96304696d1856a322b9f
MicheleMallia <mallia.michele@gmail.com>
2021-01-28 18:07:27 +0100

    added bibliography tab component

---
+++ src/app/components/lexicon-panel/text-detail/epigraphy-detail/bibliography-tab/bibliography-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bibliography-tab',
+  templateUrl: './bibliography-tab.component.html',
+  styleUrls: ['./bibliography-tab.component.scss']
+})
+export class BibliographyTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
