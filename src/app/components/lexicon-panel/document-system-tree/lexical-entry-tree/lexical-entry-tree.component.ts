6b198b6321eba32ace5dd140b665e342a0f92434
MicheleMallia <mallia.michele@gmail.com>
2021-02-12 16:21:30 +0100

    added deactivate actions event

--- src/app/components/lexicon-panel/document-system-tree/lexical-entry-tree/lexical-entry-tree.component.ts
+++ src/app/components/lexicon-panel/document-system-tree/lexical-entry-tree/lexical-entry-tree.component.ts
@@ -15,7 +15,7 @@ const actionMapping: IActionMapping = {
     click: (tree, node, $event) => {
       $event.shiftKey
         ? TREE_ACTIONS.TOGGLE_ACTIVE_MULTI(tree, node, $event)
-        : TREE_ACTIONS.TOGGLE_ACTIVE(tree, node, $event);
+        : TREE_ACTIONS.TOGGLE_ACTIVE(tree, node, $event)
     }
   },
   keys: {
