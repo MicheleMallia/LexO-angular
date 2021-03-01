0ba4ac767129521726e420dba5dbe245221bbd40
MicheleMallia <mallia.michele@gmail.com>
2021-03-01 10:16:22 +0100

    add sense input component

---
+++ src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/sense-input/sense-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sense-input',
+  templateUrl: './sense-input.component.html',
+  styleUrls: ['./sense-input.component.scss']
+})
+export class SenseInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
