566254020b611a5add379e722a04d64d6467a3cd
MicheleMallia <mallia.michele@gmail.com>
2021-04-06 18:15:29 +0200

    added autocomplete form

--- src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-form/core-form.component.ts
+++ src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-form/core-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService, Person } from './data.service';
+
 
 @Component({
   selector: 'app-core-form',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoreFormComponent implements OnInit {
 
-  constructor() { }
+  switchInput = false;
 
-  ngOnInit(): void {
-  }
+  people: Person[] = [];
+    peopleLoading = false;
+
+    constructor(
+        private dataService: DataService) {
+    }
+
+    ngOnInit() {
+        this.loadPeople();
+    }
+
+    private loadPeople() {
+        this.peopleLoading = true;
+        this.dataService.getPeople().subscribe(x => {
+            this.people = x;
+            this.peopleLoading = false;
+        });
+    }
 
 }
