a00c4efda97149f24a72b6e22c8731f4a4e59383
MicheleMallia <mallia.michele@gmail.com>
2021-04-16 16:19:29 +0200

    added lexicon entries service

--- src/app/services/lexical-entries.service.ts
+++ src/app/services/lexical-entries.service.ts
@@ -1,24 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LexicalEntriesService {
 
-  private _data : BehaviorSubject<object> = new BehaviorSubject(null);
-  //private _siblings : BehaviorSubject<object[]> = new BehaviorSubject(null);
-  
-  item$ = this._data.asObservable();
-  //sub$ = this._siblings.asObservable(); 
+  private _data: BehaviorSubject<object> = new BehaviorSubject(null);
 
-  constructor() { }
+  //private baseUrl = "https://licodemo.ilc.cnr.it/LexO-backend/service/lexicon/"
+  private baseUrl = "/LexO-backend/service/lexicon/"
+
+  item$ = this._data.asObservable();
+
+  constructor(private http: HttpClient) { }
 
   sendToCoreTab(object: object) {
     this._data.next(object)
   }
 
-  /* sendSiblings(object: object[]) {
-    this._siblings.next(object)
-  } */
+  //POST: /lexicon/lexicalEntries ---> get lexical entries list
+  getLexicalEntriesList(parameters: any): Observable<any> {
+    return this.http.post(this.baseUrl + "data/lexicalEntries", parameters);
+  }
+
+  //POST: /lexicon/lexicalSenses ---> get lexical entries list
+  getLexicalSensesList(parameters: any): Observable<any> {
+    return this.http.post(this.baseUrl + "data/lexicalSenses", parameters);
+  }
+
+  //GET /lexicon/languages --> get languages list
+  getLanguages(): Observable<any> {
+    return this.http.get(this.baseUrl + "statistics/languages?key=lexodemo");
+  }
+
+  //GET /lexicon/types --> get types list
+  getTypes(): Observable<any> {
+    return this.http.get(this.baseUrl + "statistics/types?key=lexodemo");
+  }
+
+  //GET /lexicon/authors --> get authors list
+  getAuthors(): Observable<any> {
+    return this.http.get(this.baseUrl + "statistics/authors?key=lexodemo");
+  }
+
+  //GET /lexicon/pos --> get pos list
+  getPos(): Observable<any> {
+    return this.http.get(this.baseUrl + "statistics/pos?key=lexodemo");
+  }
+
+  //GET /lexicon/states --> get states list
+  getStatus(): Observable<any> {
+    return this.http.get(this.baseUrl + "statistics/status?key=lexodemo");
+  }
 }
