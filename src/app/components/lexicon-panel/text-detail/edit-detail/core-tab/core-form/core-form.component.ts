4f7484c3ff79dc00a8d21eb220a90d459d545d37
MicheleMallia <mallia.michele@gmail.com>
2021-04-20 16:17:10 +0200

    added basic adding remove morpho trait system

--- src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-form/core-form.component.ts
+++ src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-form/core-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { DataService, Person } from './data.service';
 import { LexicalEntriesService } from '../../../../../../services/lexical-entries.service';
 import { Subscription } from 'rxjs';
@@ -17,8 +17,13 @@ export class CoreFormComponent implements OnInit {
     object : any;
     people: Person[] = [];
     peopleLoading = false;
+    counter = 0;
 
-    constructor(private dataService: DataService, private lexicalService : LexicalEntriesService) {
+    
+
+    @ViewChild('morpho_container') morphoContainer : ElementRef;
+
+    constructor(private dataService: DataService, private lexicalService : LexicalEntriesService, private renderer : Renderer2) {
     }
 
     ngOnInit() {
@@ -36,4 +41,17 @@ export class CoreFormComponent implements OnInit {
         });
     }
 
+    addMorphoTrait(){
+        const morphoHtmlModel = '<div class="d-flex my-2"> <div class="col-md-4"> <select class="form-control selectTrait" style="font-size: 12px;"> <option>'+this.counter+'</option> </select> </div> <div class="input-group col-md-7"> <select class="form-control selectTrait" style="font-size:12px;"> <option>Valori tratti Morfologici</option> </select> <div class="input-group-append ml-2"> <button class="btn btn-danger morpho-remover_'+this.counter +'" type="button" (click)="deleteMorphoTrait($event)" style="font-size: 12px; border-radius: .25rem;"><i class="fa fa-trash-alt" style="pointer-events:none"></i></button> </div> </div> </div>';
+        this.morphoContainer.nativeElement.insertAdjacentHTML('beforeend', morphoHtmlModel);
+        this.morphoContainer.nativeElement.querySelector('.morpho-remover_'+this.counter+'').addEventListener('click', this.deleteMorphoTrait.bind(this))
+        this.counter++;
+    }
+
+    deleteMorphoTrait(evt){
+
+        const ancestor = evt.target.parentNode.parentNode.parentNode;
+        this.renderer.removeChild(this.morphoContainer.nativeElement, ancestor)
+
+    }
 }
