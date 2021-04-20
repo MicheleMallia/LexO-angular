9d876ee34a9bd833aa395dd1bc4da119c766d9a4
MicheleMallia <mallia.michele@gmail.com>
2021-04-20 17:05:49 +0200

    fixed adding removing issues

--- src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-form/core-form.component.ts
+++ src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-form/core-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, SimpleChanges, ViewChild } from '@angular/core';
 import { DataService, Person } from './data.service';
 import { LexicalEntriesService } from '../../../../../../services/lexical-entries.service';
 import { Subscription } from 'rxjs';
@@ -18,10 +18,10 @@ export class CoreFormComponent implements OnInit {
     people: Person[] = [];
     peopleLoading = false;
     counter = 0;
-
-    
+    childArray = [];
 
     @ViewChild('morpho_container') morphoContainer : ElementRef;
+
 
     constructor(private dataService: DataService, private lexicalService : LexicalEntriesService, private renderer : Renderer2) {
     }
@@ -29,7 +29,15 @@ export class CoreFormComponent implements OnInit {
     ngOnInit() {
         this.loadPeople();
         this.lexicalService.coreData$.subscribe(
-            object => this.object = object
+            object => {
+                if(this.object != object){
+                    this.counter = 0;
+                    for(var i = 0; i < this.childArray.length; i++){
+                        this.renderer.removeChild(this.morphoContainer.nativeElement, this.childArray[i])
+                    }
+                }
+                this.object = object
+            }
         );
     }
 
@@ -44,6 +52,8 @@ export class CoreFormComponent implements OnInit {
     addMorphoTrait(){
         const morphoHtmlModel = '<div class="d-flex my-2"> <div class="col-md-4"> <select class="form-control selectTrait" style="font-size: 12px;"> <option>'+this.counter+'</option> </select> </div> <div class="input-group col-md-7"> <select class="form-control selectTrait" style="font-size:12px;"> <option>Valori tratti Morfologici</option> </select> <div class="input-group-append ml-2"> <button class="btn btn-danger morpho-remover_'+this.counter +'" type="button" (click)="deleteMorphoTrait($event)" style="font-size: 12px; border-radius: .25rem;"><i class="fa fa-trash-alt" style="pointer-events:none"></i></button> </div> </div> </div>';
         this.morphoContainer.nativeElement.insertAdjacentHTML('beforeend', morphoHtmlModel);
+        const children = this.morphoContainer.nativeElement.children[this.morphoContainer.nativeElement.children.length-1]
+        this.childArray.push(children);
         this.morphoContainer.nativeElement.querySelector('.morpho-remover_'+this.counter+'').addEventListener('click', this.deleteMorphoTrait.bind(this))
         this.counter++;
     }
