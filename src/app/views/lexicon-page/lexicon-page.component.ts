b5d4a35bbfabfd74c67643463939f57b9b7827e1
MicheleMallia <mallia.michele@gmail.com>
2021-04-20 11:49:31 +0200

    create child input for passing data

--- src/app/views/lexicon-page/lexicon-page.component.ts
+++ src/app/views/lexicon-page/lexicon-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LexicalEntriesService } from 'src/app/services/lexical-entries.service';
 
 @Component({
   selector: 'app-lexicon-page',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lexicon-page.component.scss']
 })
 export class LexiconPageComponent implements OnInit {
+  subscription: Subscription;
+  object: any;
+  constructor(private lexicalService: LexicalEntriesService) { }
 
-  constructor() { }
-
+  notes = '';
+  link = [];
   ngOnInit(): void {
+    this.lexicalService.rightPanelData$.subscribe(
+      object => {
+        this.object = object;
+        if(this.object !=null){
+          this.notes = this.object.note;
+          this.link = this.object.links[0];
+        }else{
+          this.notes = null;
+          this.link = null;
+        }
+      }
+    );
   }
 
 }
