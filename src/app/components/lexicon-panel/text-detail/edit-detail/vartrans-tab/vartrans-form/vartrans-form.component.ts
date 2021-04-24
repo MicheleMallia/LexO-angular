6faf70825c4244e4221761b6777cb9cb4ee18181
MicheleMallia <mallia.michele@gmail.com>
2021-04-24 02:15:18 +0200

    builded base reactive form for vartrans

--- src/app/components/lexicon-panel/text-detail/edit-detail/vartrans-tab/vartrans-form/vartrans-form.component.ts
+++ src/app/components/lexicon-panel/text-detail/edit-detail/vartrans-tab/vartrans-form/vartrans-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, ComponentFactoryResolver, OnInit, Renderer2, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { LexicalEntriesService } from 'src/app/services/lexical-entries.service';
 import { DataService, Person } from '../../core-tab/core-form/data.service';
 
 import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-vartrans-form',
@@ -18,14 +19,34 @@ export class VartransFormComponent implements OnInit {
   people: Person[] = [];
   peopleLoading = false;
   counter = 0;
-  componentRef : any;
+  componentRef: any;
+
+  vartransForm = new FormGroup({
+    label: new FormControl(''),
+    translatableAs: new FormArray([this.createTranslatableAs()]),
+    lexicalRelationDirect: new FormArray([this.createLexicalRelationDirect()]),
+    lexicalRelationIndirect: new FormArray([this.createLexicalRelationIndirect()])
+  })
+
+  translatableAs: FormArray;
+  lexicalRelationDirect: FormArray;
+  lexicalRelationIndirect: FormArray;
 
 
-
-  constructor(private dataService: DataService, private lexicalService: LexicalEntriesService) {
+  constructor(private dataService: DataService, private lexicalService: LexicalEntriesService, private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.vartransForm = this.formBuilder.group({
+      label: '',
+      translatableAs: this.formBuilder.array([this.createTranslatableAs()]),
+      lexicalRelationDirect: this.formBuilder.array([this.createLexicalRelationDirect()]),
+      lexicalRelationIndirect: this.formBuilder.array([this.createLexicalRelationIndirect()])
+    })
+
+    this.onChanges();
+
+    console.log(this.vartransForm)
     this.loadPeople();
     this.lexicalService.coreData$.subscribe(
       object => {
@@ -42,5 +63,38 @@ export class VartransFormComponent implements OnInit {
     });
   }
 
-  
+  onChanges(): void {
+    this.vartransForm.valueChanges.pipe(debounceTime(200)).subscribe(searchParams => {
+      console.log(searchParams)
+    })
+  }
+
+  createTranslatableAs(): FormGroup {
+    return this.formBuilder.group({
+      entity: ''
+    })
+  }
+
+  createLexicalRelationDirect(): FormGroup {
+    return this.formBuilder.group({
+      relation: '',
+      entity: ''
+    })
+  }
+
+  createLexicalRelationIndirect(): FormGroup {
+    return this.formBuilder.group({
+      a_entity: '',
+      relation: '',
+      b_entity: '',
+      sub_rel: new FormArray([this.createSubLexicalRelationIndirect()])
+    })
+  }
+
+  createSubLexicalRelationIndirect(): FormGroup {
+    return this.formBuilder.group({
+      sub_relation: 'eee',
+      sub_entity: ''
+    })
+  }
 }
