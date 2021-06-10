dc737335b212023c3a353689ae3c51371c3bbfcc
MicheleMallia <mallia.michele@gmail.com>
2021-06-10 10:55:25 +0200

    added a system for searching node when edited

--- src/app/components/lexicon-panel/document-system-tree/document-system-tree.component.ts
+++ src/app/components/lexicon-panel/document-system-tree/document-system-tree.component.ts
@@ -9,41 +9,79 @@ import { LexicalEntriesService } from 'src/app/services/lexical-entries/lexical-
 
 export class DocumentSystemTreeComponent implements OnInit {
 
-  switcher= false;
+  switcher = false;
   @ViewChild('lexTree') lexTree: any;
-  
+
   constructor(private lexicalService: LexicalEntriesService) { }
 
   ngOnInit(): void {
+    this.lexicalService.refreshAfterEdit$.subscribe(
+      data => {
+        this.refreshAfterEdit(data);
+      }
+    )
   }
 
-  updateTreeParent(){
+  updateTreeParent() {
     this.lexTree.updateTreeView();
   }
 
-  switchLabel(){
+  switchLabel() {
     this.lexTree.labelView = !this.lexTree.labelView;
     this.lexTree.idView = !this.lexTree.idView;
     this.switcher = !this.switcher;
   }
 
-  newLexicalEntry(){
-    let parameters = this.lexTree.getParameters();
+  refreshAfterEdit(data) {
+    
+    if(data != null){
+      setTimeout(() => {
+        let newLexEntryLabel = data['label'];
+        let parameters = this.lexTree.getParameters();
+        parameters['text'] = newLexEntryLabel + "~0.5";
+        this.lexTree.lexicalEntriesFilter(parameters);
+        this.lexTree.lexicalEntryTree.treeModel.update();
+        this.lexTree.updateTreeView();
+        
+        setTimeout(() => {
+          this.lexTree.lexicalEntryTree.treeModel.getNodeBy(
+            function (x) {
+              console.log(x)
+              if (x.data.label == newLexEntryLabel) {
+                x.setActiveAndVisible()
+                return true;
+              } else {
+                return false;
+              }
+            }
+          );
+        }, 500);
+      }, 100);
+    }
+    
+
+  }
+
+  newLexicalEntry() {
+
     this.lexicalService.newLexicalEntry().subscribe(
       data => {
         console.log(data);
         setTimeout(() => {
+          let newLexEntryLabel = data['label'];
+          let parameters = this.lexTree.getParameters();
+          parameters['text'] = newLexEntryLabel + "~0.5";
           this.lexTree.lexicalEntriesFilter(parameters);
           this.lexTree.lexicalEntryTree.treeModel.update();
           this.lexTree.updateTreeView();
 
           setTimeout(() => {
             this.lexTree.lexicalEntryTree.treeModel.getNodeBy(
-              function(x){
-                if(x.data.lexicalEntryInstanceName == data['lexicalEntryInstanceName']){
+              function (x) {
+                if (x.data.lexicalEntryInstanceName == data['lexicalEntryInstanceName']) {
                   x.setActiveAndVisible()
                   return true;
-                }else {
+                } else {
                   return false;
                 }
               }
