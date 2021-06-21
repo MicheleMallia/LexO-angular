150dfacd2f13dfe45a3e69ed07a8d5cebd64d131
MicheleMallia <mallia.michele@gmail.com>
2021-06-21 12:13:56 +0200

    call service for retireve sameAs and SeeAlso data

--- src/app/components/link-panel/link-panel.component.ts
+++ src/app/components/link-panel/link-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { LexicalEntriesService } from 'src/app/services/lexical-entries/lexical-entries.service';
 
 @Component({
   selector: 'app-link-panel',
@@ -9,21 +10,44 @@ export class LinkPanelComponent implements OnInit {
 
   @Input() linkData: any[] | any;
   
-  seeAlsoData : any;
-  sameAsData : any;
+  seeAlsoData : {};
+  sameAsData : {};
   counterElement = 0;
-  constructor() { }
+  object : any;
+  constructor(private lexicalService: LexicalEntriesService) { }
 
   ngOnInit(): void {
   }
 
   ngOnChanges(changes: SimpleChanges){
     if(changes.linkData.currentValue != null){
-      this.seeAlsoData = this.linkData.elements[0];
-      this.sameAsData = this.linkData.elements[1];
-      /* console.log(this.seeAlsoData) */
-      this.counterElement = this.seeAlsoData.count + this.sameAsData.count;
+      
+      this.object = changes.linkData.currentValue;
+      this.sameAsData = this.object;
+      this.seeAlsoData = this.object;
       console.log(changes.linkData.currentValue)
+
+      //TODO: richiamare il servizio per il recupero dei dati relativi a sameAs e seeAlso
+      if(this.object.lexicalEntryInstanceName != undefined){
+        let lexId = this.object.lexicalEntryInstanceName;
+        this.lexicalService.getLexEntryLinguisticRelation(lexId, 'sameAs').subscribe(
+          data=>{
+            console.log(data)
+          }, error=>{
+            console.log(error);
+            
+          }
+        )
+
+        this.lexicalService.getLexEntryLinguisticRelation(lexId, 'seeAlso').subscribe(
+          data=>{
+            console.log(data)
+          }, error=>{
+            console.log(error);
+            
+          }
+        )
+      }
       
     }else{
       this.counterElement = 0;
