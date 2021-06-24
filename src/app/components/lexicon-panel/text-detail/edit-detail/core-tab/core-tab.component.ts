35c278afc6be713b2ed1032fb1805f5d0a8735f6
MicheleMallia <mallia.michele@gmail.com>
2021-06-24 16:14:58 +0200

    addNewSense method updated

--- src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-tab.component.ts
+++ src/app/components/lexicon-panel/text-detail/edit-detail/core-tab/core-tab.component.ts
@@ -388,6 +388,29 @@ export class CoreTabComponent implements OnInit {
           //this.lexicalService.refreshLexEntryTree();
         }
       )
+    }else if(this.isSense){
+      let parentNodeInstanceName = this.object.parentNodeInstanceName;
+      let parentNodeLabel = this.object.parentNodeLabel;
+      console.log(this.object);
+      this.lexicalService.createNewForm(parentNodeInstanceName).subscribe(
+        data=>{
+          data['request'] = 1;
+          data['parentNode'] = parentNodeLabel;
+          data['whatToSearch'] = 'form';
+          data['instanceName'] = data['formInstanceName'];
+          if(data['creator'] == this.object.creator){
+            data['flagAuthor'] = false;
+          }else{
+            data['flagAuthor'] = true;
+          }
+          this.searchIconSpinner = false;
+          this.lexicalService.refreshAfterEdit(data);
+          //this.lexicalService.refreshLexEntryTree();
+        },error=> {
+          this.searchIconSpinner = false;
+          //this.lexicalService.refreshLexEntryTree();
+        }
+      )
     }
     
   }
@@ -395,14 +418,69 @@ export class CoreTabComponent implements OnInit {
   addNewSense(){
     this.searchIconSpinner = true;
     let lexicalId = this.object.lexicalEntryInstanceName;
-    this.lexicalService.createNewSense(lexicalId).subscribe(
-      data=>{
-        this.searchIconSpinner = false;
-        this.lexicalService.refreshLexEntryTree();
-      },error=> {
-        this.searchIconSpinner = false;
-        this.lexicalService.refreshLexEntryTree();
-      }
-    )
+    if(this.isLexicalEntry){
+      let lexicalId = this.object.lexicalEntryInstanceName;
+      this.lexicalService.createNewSense(lexicalId).subscribe(
+        data=>{
+          data['request'] = 1;
+          data['parentNode'] = this.object.label; 
+          data['whatToSearch'] = 'sense';
+          data['instanceName'] = data['senseInstanceName'];
+          this.searchIconSpinner = false;
+          this.lexicalService.refreshAfterEdit(data);
+          //this.lexicalService.refreshLexEntryTree();
+        },error=> {
+          this.searchIconSpinner = false;
+          //this.lexicalService.refreshLexEntryTree();
+        }
+      )
+    }else if(this.isSense){
+      let parentNodeInstanceName = this.object.parentNodeInstanceName;
+      let parentNodeLabel = this.object.parentNodeLabel;
+      console.log(this.object);
+      this.lexicalService.createNewSense(parentNodeInstanceName).subscribe(
+        data=>{
+          data['request'] = 2;
+          data['parentNode'] = parentNodeLabel;
+          data['whatToSearch'] = 'sense';
+          data['instanceName'] = data['senseInstanceName'];
+          if(data['creator'] == this.object.creator){
+            data['flagAuthor'] = false;
+          }else{
+            data['flagAuthor'] = true;
+          }
+          this.searchIconSpinner = false;
+          this.lexicalService.refreshAfterEdit(data);
+          //this.lexicalService.refreshLexEntryTree();
+        },error=> {
+          this.searchIconSpinner = false;
+          //this.lexicalService.refreshLexEntryTree();
+        }
+      )
+    }else if(this.isForm){
+      let parentNodeInstanceName = this.object.parentNodeInstanceName;
+      let parentNodeLabel = this.object.parentNodeLabel;
+      console.log(this.object);
+      this.lexicalService.createNewSense(parentNodeInstanceName).subscribe(
+        data=>{
+          data['request'] = 1;
+          data['parentNode'] = parentNodeLabel;
+          data['whatToSearch'] = 'sense';
+          data['instanceName'] = data['senseInstanceName'];
+          if(data['creator'] == this.object.creator){
+            data['flagAuthor'] = false;
+          }else{
+            data['flagAuthor'] = true;
+          }
+          this.searchIconSpinner = false;
+          this.lexicalService.refreshAfterEdit(data);
+          //this.lexicalService.refreshLexEntryTree();
+        },error=> {
+          this.searchIconSpinner = false;
+          //this.lexicalService.refreshLexEntryTree();
+        }
+      )
+    }
+
   }
 }
