c27158cd1a1feb5bc39c321c683fa6df737d5542
MicheleMallia <mallia.michele@gmail.com>
2021-08-16 17:59:39 +0200

    auto switching when clicking a lexical entry

--- src/app/components/lexicon-panel/text-detail/edit-detail/edit-detail.component.ts
+++ src/app/components/lexicon-panel/text-detail/edit-detail/edit-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { LexicalEntriesService } from 'src/app/services/lexical-entries/lexical-entries.service';
 
 @Component({
   selector: 'app-edit-detail',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditDetailComponent implements OnInit {
 
-  constructor() { }
+  showTrigger = false;
+  @ViewChild('navtabs') navtabs: ElementRef; 
+  @ViewChild('navcontent') navcontent: ElementRef; 
+
+  constructor(private lexicalService: LexicalEntriesService) { }
 
   ngOnInit(): void {
+    
+    this.lexicalService.coreData$.subscribe(
+      object => {
+        if(object != null){
+          if(object['lexicalEntryInstanceName'] != undefined ||
+             object['formInstanceName'] != undefined){
+
+            var navTabLinks = this.navtabs.nativeElement.querySelectorAll('a')
+            console.log(navTabLinks)
+            navTabLinks.forEach(element => {
+              console.log(element)
+              if(element.text == 'Core'){
+                element.classList.add('active')
+              }else{
+                element.classList.remove('active')
+              }
+            });
+
+            var navContent = this.navcontent.nativeElement.querySelectorAll('.tab-pane');
+            console.log(navContent)
+            navContent.forEach(element => {
+              console.log(element)
+              if(element.id == 'core'){
+                element.classList.add('active')
+                element.classList.add('show')
+              }else{
+                element.classList.remove('active')
+                element.classList.remove('show')
+              }
+            });
+            this.showTrigger = true;
+          }else {
+            this.showTrigger = false;
+          }
+        }
+      }
+    );
   }
 
 }
