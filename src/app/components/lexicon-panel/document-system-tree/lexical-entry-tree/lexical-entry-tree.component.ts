c0ea0cb5ef2d7e17436097171e2d919d880f4b56
MicheleMallia <mallia.michele@gmail.com>
2022-04-05 15:21:13 +0200

    added gpl 3.0

--- src/app/components/lexicon-panel/document-system-tree/lexical-entry-tree/lexical-entry-tree.component.ts
+++ src/app/components/lexicon-panel/document-system-tree/lexical-entry-tree/lexical-entry-tree.component.ts
@@ -1,3 +1,15 @@
+﻿/*
+  © Copyright 2021-2022  Istituto di Linguistica Computazionale "A. Zampolli", Consiglio Nazionale delle Ricerche, Pisa, Italy.
+ 
+This file is part of EpiLexo.
+
+EpiLexo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+EpiLexo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with EpiLexo. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { ApplicationRef, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { TreeNode, TreeModel, TREE_ACTIONS, KEYS, IActionMapping, ITreeOptions, ITreeState } from '@circlon/angular-tree-component';
 import { formTypeEnum, LexicalEntryRequest, searchModeEnum, typeEnum } from './interfaces/lexical-entry-interface'
--- src/app/components/lexicon-panel/text-detail/epigraphy-detail/epigraphy-tab/epigraphy-tab.component.ts
+++ src/app/components/lexicon-panel/text-detail/epigraphy-detail/epigraphy-tab/epigraphy-tab.component.ts
@@ -1,3 +1,15 @@
+﻿/*
+  © Copyright 2021-2022  Istituto di Linguistica Computazionale "A. Zampolli", Consiglio Nazionale delle Ricerche, Pisa, Italy.
+ 
+This file is part of EpiLexo.
+
+EpiLexo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+EpiLexo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with EpiLexo. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import * as Recogito from '@recogito/recogito-js';
